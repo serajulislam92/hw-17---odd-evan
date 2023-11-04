74fe7b69edf3763f22e42afbcf9582d749f9b9d8
serajulislam92 <144261626+serajulislam92@users.noreply.github.com>
2023-11-04 06:39:40 -0700

    Add files via upload

---
+++ js/apps.js
@@ -0,0 +1,11 @@
+let num = Number(prompt());
+
+function js(a,b){
+    if (num % 2 ==0){
+        document.write("This number is evan")
+    } else{
+        document.write("This number is odd")
+    }
+}
+
+js();
